ab64be64d65d6468cb5a6df305721b9aa1dad2c3
davidsnts <davidwallacedossantos@gmail.com>
2024-05-14 20:18:05 -0300

    Criação do projeto asp.net mvc

---
+++ GestorEquipamentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestorEquipamentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
